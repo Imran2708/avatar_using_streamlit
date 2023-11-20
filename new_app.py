27a33302607219a95f17fc5d6e958fe00116bcb2
Swathi S <105411200+s-swathib@users.noreply.github.com>
2023-11-20 17:56:01 +0530

    Update new_app.py
    changes in passing values

--- new_app.py
+++ new_app.py
@@ -44,7 +44,7 @@ def submit_synthesis(query,avatar,voice):
         'description': DESCRIPTION,
         "textType": "PlainText",
         'synthesisConfig': {
-            "voice": str(voice),
+            "voice": voice,
         },
         # Replace with your custom voice name and deployment ID if you want to use custom voice.
         # Multiple voices are supported, the mixture of custom voices and platform voices is allowed.
@@ -54,11 +54,11 @@ def submit_synthesis(query,avatar,voice):
         },
         "inputs": [
             {
-                "text": str(query),
+                "text": query,
             },
         ],        "properties": {
             "customized": False, # set to True if you want to use customized avatar
-            "talkingAvatarCharacter": str(avatar),  # talking avatar character
+            "talkingAvatarCharacter": avatar,  # talking avatar character
             "talkingAvatarStyle": "graceful-sitting",  # talking avatar style, required for prebuilt avatar, optional for custom avatar
             "videoFormat": "mp4",  # mp4 or webm, webm is required for transparent background
             "videoCodec": "vp9",  # hevc, h264 or vp9, vp9 is required for transparent background; default is hevc
@@ -130,3 +130,4 @@ if __name__ == '__main__':
                         break
                     elif status == 'Failed':
                         st.markdown("Something is wrong!")
+                        break
