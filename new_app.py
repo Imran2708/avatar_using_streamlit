1e70b9333ed13197cdbc38f5a680f74e44a82914
Swathi S <105411200+s-swathib@users.noreply.github.com>
2023-11-20 19:39:09 +0530

    Update new_app.py

--- new_app.py
+++ new_app.py
@@ -66,55 +66,45 @@ def submit_synthesis(query,avatar,voice):
             "backgroundColor": "transparent",
         }
     }
- 
     response = requests.post(url, json.dumps(payload), headers=header)
+    st.info('All good 1')
     #if response.status_code < 400:
     #    logger.info('Batch avatar synthesis job submitted successfully')
     #    logger.info(f'Job ID: {response.json()["id"]}')
     return response.json()["id"]
     #else:
     #    logger.error(f'Failed to submit batch avatar synthesis job: {response.text}')
-
-def get_synthesis(job_id):
-    url = f'https://{SERVICE_REGION}.{SERVICE_HOST}/api/texttospeech/3.1-preview1/batchsynthesis/talkingavatar/{job_id}'
-    header = {
-        'Ocp-Apim-Subscription-Key': SUBSCRIPTION_KEY
-    }
-    response = requests.get(url, headers=header)
-    if response.status_code < 400:
-        logger.debug('Get batch synthesis job successfully')
-        logger.debug(response.json())
-        if response.json()['status'] == 'Succeeded':
-    #        logger.info(f'Batch synthesis job succeeded, download URL: {response.json()["outputs"]["result"]}')
-            url1 = {response.json()["outputs"]["result"]}
-            status = response.json()['status']
-            return status , url1
-        else:
-    #       logger.error(f'Failed to get batch synthesis job: {response.text}')
-            return response.json()['status']
- 
  
 if __name__ == '__main__':
     query = st.text_area("Type a sentence ", value= "Hi, I'm a virtual assistant created by Microsoft.", on_change=clear_submit)
+    
     col1, col2= st.columns(2)
+    
     with col1:
         avatar = st.radio('Select an avatar character:', key="visibility", options=["lisa"],)
+    
     with col2:
         voice = st.selectbox('Select voice:', ('en-US-JennyNeural', 'en-US-GuyNeural', 'en-US-AriaNeural', 'en-US-DavisNeural', 'en-US-AmberNeural', 'en-US-AndrewNeural', 'en-US-AshleyNeural', 'en-US-BrandonNeural','en-US-BrianNeural', 'en-US-ChristopherNeural','en-US-CoraNeural','en-US-ElizabethNeural','en-US-EmmaNeural','en-US-EricNeural','en-US-JacobNeural','en-US-JaneNeural','en-US-JasonNeural','en-US-MichelleNeural','en-US-MonicaNeural'),)
+    
     generate = st.button('Generate video')
+    
     if generate or st.session_state.get("submit"):
         if not query:
             st.error("Please enter a statement!")
         else:
             st.session_state["submit"] = True
+            
             job_id = submit_synthesis(query,avatar,voice)
+            st.info('All good 2')
             if job_id is not None:
                 timeout = time.time() +120 #set a timeout of 2 minutes
                 while time.time() < timeout:
+                    st.info('All good 3')
                     url = f'https://{SERVICE_REGION}.{SERVICE_HOST}/api/texttospeech/3.1-preview1/batchsynthesis/talkingavatar/{job_id}' 
                     header = {'Ocp-Apim-Subscription-Key': SUBSCRIPTION_KEY}
                     response = requests.get(url, headers=header)
                     if response.status_code < 400:
+                        st.info('All good 4')
                         logger.debug('Get batch synthesis job successfully')
                         logger.debug(response.json())
                         if response.json()['status'] == 'Succeeded':
@@ -124,16 +114,20 @@ if __name__ == '__main__':
                     else:
                         st.error(f'Failed to get batch synthesis job: {response.text}')
                     
+                    
                     status = response.json()['status']
-             
+
+                    st.info('All good 5')
                     if status == 'Succeeded':
                         #logger.info('batch avatar synthesis job succeeded')
+                        st.info('All good 6')
                         st.video(url1,format="mp4")
                         break
                     elif status == 'Failed':
                         st.error('Batch avatar synthesis job failed')
                         break
                     else:
+                        st.info('All good 7')
                         st.info(f'batch avatar synthesis job is still running, status [{status}]')
                         time.sleep(10)
                 else:
