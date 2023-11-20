52e91580b5dc3217e991c35675026b8056e3aae2
Swathi S <105411200+s-swathib@users.noreply.github.com>
2023-11-20 18:58:17 +0530

    Update new_app.py

--- new_app.py
+++ new_app.py
@@ -95,7 +95,7 @@ def get_synthesis(job_id):
  
  
 if __name__ == '__main__':
-    query = st.text_input("Type a sentence ", value= "Hi, I'm a virtual assistant created by Microsoft.", on_change=clear_submit)
+    query = st.text_area("Type a sentence ", value= "Hi, I'm a virtual assistant created by Microsoft.", on_change=clear_submit)
     col1, col2= st.columns(2)
     with col1:
         avatar = st.radio('Select an avatar character:', key="visibility", options=["lisa"],)
