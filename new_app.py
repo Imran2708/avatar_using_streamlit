f07a203692c2440341a602b9c5acaa36215d1eb1
Swathi S <105411200+s-swathib@users.noreply.github.com>
2023-11-21 10:48:16 +0530

    Update new_app.py

--- new_app.py
+++ new_app.py
@@ -1,30 +1,30 @@
-import streamlit as st 
+import streamlit as st
 import json
 import logging
 import os
 import sys
 import time
 from pathlib import Path
- 
+
 import requests
 
 st.title("Text to speech Avatar using Streamlit")
 
-#st.markdown('## Key Metrics')
+st.markdown('## Key Metrics')
 
 logging.basicConfig(stream=sys.stdout, level=logging.INFO,  # set to logging.DEBUG for verbose output
         format="[%(asctime)s] %(message)s", datefmt="%m/%d/%Y %I:%M:%S %p %Z")
 logger = logging.getLogger(__name__)
- 
+
 # Your Speech resource key and region
 # This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
- 
+
 SUBSCRIPTION_KEY = os.getenv("SUBSCRIPTION_KEY", '381744472c174221b178ad4e561a4c60')
 SERVICE_REGION = os.getenv("SERVICE_REGION", "westus2")
- 
+
 NAME = "Simple avatar synthesis"
 DESCRIPTION = "Simple avatar synthesis description"
- 
+
 # The service host suffix.
 SERVICE_HOST = "customvoice.api.speech.microsoft.com"
 
@@ -38,13 +38,13 @@ def submit_synthesis(query,avatar,voice):
         'Ocp-Apim-Subscription-Key': SUBSCRIPTION_KEY,
         'Content-Type': 'application/json'
     }
- 
+
     payload = {
         'displayName': NAME,
         'description': DESCRIPTION,
         "textType": "PlainText",
         'synthesisConfig': {
-            "voice": str(voice),
+            "voice": voice,
         },
         # Replace with your custom voice name and deployment ID if you want to use custom voice.
         # Multiple voices are supported, the mixture of custom voices and platform voices is allowed.
@@ -54,30 +54,36 @@ def submit_synthesis(query,avatar,voice):
         },
         "inputs": [
             {
-                "text": str(query),
+                "text": query,
             },
-        ],        "properties": {
+        ],
+        "properties": {
             "customized": False, # set to True if you want to use customized avatar
-            "talkingAvatarCharacter": str(avatar),  # talking avatar character
+            "talkingAvatarCharacter": avatar,  # talking avatar character
             "talkingAvatarStyle": "graceful-sitting",  # talking avatar style, required for prebuilt avatar, optional for custom avatar
-            "videoFormat": "mp4",  # mp4 or webm, webm is required for transparent background
+            "videoFormat": "webm",  # mp4 or webm, webm is required for transparent background
             "videoCodec": "vp9",  # hevc, h264 or vp9, vp9 is required for transparent background; default is hevc
             "subtitleType": "soft_embedded",
             "backgroundColor": "transparent",
         }
     }
+
     response = requests.post(url, json.dumps(payload), headers=header)
-    st.info('All good 1')
     if response.status_code < 400:
         logger.info('Batch avatar synthesis job submitted successfully')
+        st.info('Batch avatar synthesis job submitted successfully')
         logger.info(f'Job ID: {response.json()["id"]}')
         return response.json()["id"]
     else:
         logger.error(f'Failed to submit batch avatar synthesis job: {response.text}')
- 
+        st.error(f'Failed to submit batch avatar synthesis job: {response.text}')
+  
+  
 if __name__ == '__main__':
+    #query = "Hi i'm virtual assistant from microsoft"
     query = st.text_area("Type a sentence ", value= "Hi, I'm a virtual assistant created by Microsoft.", on_change=clear_submit)
-    
+    #avatar = 'lisa'
+    #voice = 'en-US-JennyNeural'
     col1, col2= st.columns(2)
     
     with col1:
@@ -86,52 +92,44 @@ if __name__ == '__main__':
     with col2:
         voice = st.selectbox('Select voice:', ('en-US-JennyNeural', 'en-US-GuyNeural', 'en-US-AriaNeural', 'en-US-DavisNeural', 'en-US-AmberNeural', 'en-US-AndrewNeural', 'en-US-AshleyNeural', 'en-US-BrandonNeural','en-US-BrianNeural', 'en-US-ChristopherNeural','en-US-CoraNeural','en-US-ElizabethNeural','en-US-EmmaNeural','en-US-EricNeural','en-US-JacobNeural','en-US-JaneNeural','en-US-JasonNeural','en-US-MichelleNeural','en-US-MonicaNeural'),)
     
-    generate = st.button('Generate video')
+    generate = st.button('Generate video') 
     
     if generate or st.session_state.get("submit"):
         if not query:
             st.error("Please enter a statement!")
         else:
             st.session_state["submit"] = True
-            
             job_id = submit_synthesis(query,avatar,voice)
-            st.info('All good 2')
             if job_id is not None:
-                timeout = time.time() +120 #set a timeout of 2 minutes
+                timeout = time.time()+120
                 while time.time() < timeout:
-                    st.info('All good 3')
                     url = f'https://{SERVICE_REGION}.{SERVICE_HOST}/api/texttospeech/3.1-preview1/batchsynthesis/talkingavatar/{job_id}' 
-                    header = {'Ocp-Apim-Subscription-Key': SUBSCRIPTION_KEY}
+                    header = {
+                        'Ocp-Apim-Subscription-Key': SUBSCRIPTION_KEY 
+                    }
                     response = requests.get(url, headers=header)
                     if response.status_code < 400:
-                        st.info('All good 4')
                         logger.debug('Get batch synthesis job successfully')
                         logger.debug(response.json())
-                        status = response.json()['status']
-                        st.info(f'batch avatar status [{status}]')
                         if response.json()['status'] == 'Succeeded':
-                            st.info('All good 5')
                             logger.info(f'Batch synthesis job succeeded, download URL: {response.json()["outputs"]["result"]}')
                             st.info(f'Batch synthesis job succeeded, download URL: {response.json()["outputs"]["result"]}')
                             url1 = response.json()["outputs"]["result"]
                     else:
-                        st.error(f'Failed to get batch synthesis job: {response.text}')
-                    
-                    
+                        logger.error(f'Failed to get batch synthesis job: {response.text}')
+                        st.error(f'Failed to get batch synthesis job : {response.text}')
                     status = response.json()['status']
-
-                    st.info('All good 6')
+            
                     if status == 'Succeeded':
+                        logger.info('batch avatar synthesis job succeeded')
                         st.info('batch avatar synthesis job succeeded')
-                        st.info('All good 7')
                         st.video(url1,format="mp4")
                         break
                     elif status == 'Failed':
+                        logger.error('batch avatar synthesis job failed')
                         st.error('Batch avatar synthesis job failed')
                         break
                     else:
-                        st.info('All good 8')
+                        logger.info(f'batch avatar synthesis job is still running, status [{status}]')
                         st.info(f'batch avatar synthesis job is still running, status [{status}]')
-                        time.sleep(10)
-                else:
-                    st.error('Timeout: Batch avatar synthesis job took too long')
+                        time.sleep(5)
